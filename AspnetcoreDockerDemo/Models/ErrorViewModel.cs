e8fd44a714a421102677cbd4dd59944baff90511
capad <wizard1946@163.com>
2018-06-04 14:17:49 +0800

    Add project files.

---
+++ AspnetcoreDockerDemo/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace AspnetcoreDockerDemo.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
